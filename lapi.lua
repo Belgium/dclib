84dcc0aeb7ce6a72cec3a15d40bbe02fa0dfd7f9
tarjoilija <admin@awly.org>
2013-09-22 21:12:17 +0300

    Disabled overloading

--- lapi.lua
+++ lapi.lua
@@ -1,4 +1,9 @@
-dofile("sys/lua/lapi/player.lua")
-dofile("sys/lua/lapi/object.lua")
-dofile("sys/lua/lapi/image.lua")
-dofile("sys/lua/lapi/hook.lua")
+if not lapi then
+    dofile("sys/lua/lapi/player.lua")
+    dofile("sys/lua/lapi/object.lua")
+    dofile("sys/lua/lapi/image.lua")
+    dofile("sys/lua/lapi/hook.lua")
+    lapi = {
+        version = "0.1"
+    }
+end
