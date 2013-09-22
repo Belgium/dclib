13c7ca47115326ac66258e43ed517037da6799e7
tarjoilija <admin@awly.org>
2013-09-22 21:35:00 +0300

    Changed from Hook.add(..) to Hook(..)

--- hook.lua
+++ hook.lua
@@ -50,62 +50,64 @@ Hook.support = {
 }
 Hook.support['break'] = true
 
-function Hook.add(hook, func, prio)
-    local id = 'Hook_' .. Hook.count
-    
-    if Hook.support[hook] then
-        if hook == 'kill' then
-            Hook[id] = function(killer, victim, ...)
-                return _G[func](Player(killer), Player(victim), unpack(arg))
-            end
-        elseif hook == 'hitzone' then
-            Hook[id] = function(img, ply, obj)
-                return _G[func](Image(img), Player(ply), Object(obj), unpack(arg))
-            end
-        elseif hook == 'objectdamage' then
-            Hook[id] = function(id, dmg, ply)
-                return _G[func](Object(id), dmg, Player(ply))
-            end
-        elseif hook == 'objectkill' then
-            Hook[id] = function(id, ply)
-                return _G[func](Object(id), Player(ply))
-            end
-        elseif hook == 'objectupgrade' then
-            Hook[id] = function(id, ply, ...)
-                return _G[func](Object(id), Player(ply), unpack(arg))
-            end
-        elseif hook == 'break' then
-            Hook[id] = function(x, y, ply)
-                return _G[func](x, y, Player(ply))
-            end
-        elseif hook == 'rcon' then
-            Hook[id] = function(cmds, ply, ...)
-                return _G[func](cmds, Player(ply), unpack(arg))
+setmetatable(Hook, {
+    __call = function(_, hook, func, prio)
+        local id = 'Hook_' .. Hook.count
+        
+        if Hook.support[hook] then
+            if hook == 'kill' then
+                Hook[id] = function(killer, victim, ...)
+                    return _G[func](Player(killer), Player(victim), unpack(arg))
+                end
+            elseif hook == 'hitzone' then
+                Hook[id] = function(img, ply, obj)
+                    return _G[func](Image(img), Player(ply), Object(obj), unpack(arg))
+                end
+            elseif hook == 'objectdamage' then
+                Hook[id] = function(id, dmg, ply)
+                    return _G[func](Object(id), dmg, Player(ply))
+                end
+            elseif hook == 'objectkill' then
+                Hook[id] = function(id, ply)
+                    return _G[func](Object(id), Player(ply))
+                end
+            elseif hook == 'objectupgrade' then
+                Hook[id] = function(id, ply, ...)
+                    return _G[func](Object(id), Player(ply), unpack(arg))
+                end
+            elseif hook == 'break' then
+                Hook[id] = function(x, y, ply)
+                    return _G[func](x, y, Player(ply))
+                end
+            elseif hook == 'rcon' then
+                Hook[id] = function(cmds, ply, ...)
+                    return _G[func](cmds, Player(ply), unpack(arg))
+                end
+            else
+                Hook[id] = function(id, ...) -- create a wrapper function
+                    return _G[func](Player(id), unpack(arg))
+                end
             end
         else
-            Hook[id] = function(id, ...) -- create a wrapper function
-                return _G[func](Player(id), unpack(arg))
-            end
+            Hook[id] = _G[func]
         end
-    else
-        Hook[id] = _G[func]
+        
+        if prio then
+            addhook(hook, 'Hook.' .. id, prio)
+        else
+            addhook(hook, 'Hook.' .. id)
+        end
+        Hook.count = Hook.count + 1
+        
+        local tbl = {
+            hook = hook,
+            func = func,
+            prio = prio,
+            id = 'Hook.' .. id
+        }
+        return setmetatable(tbl, Hook.mt)
     end
-    
-    if prio then
-        addhook(hook, 'Hook.' .. id, prio)
-    else
-        addhook(hook, 'Hook.' .. id)
-    end
-    Hook.count = Hook.count + 1
-    
-    local tbl = {
-        hook = hook,
-        func = func,
-        prio = prio,
-        id = 'Hook.' .. id
-    }
-    return setmetatable(tbl, Hook.mt)
-end
+})
 
 function Hook.mt:remove()
     freehook(self.hook, self.id)
