b4703a7da1be940f02d061378c456296e0f8bc50
tarjoilija <admin@awly.org>
2013-09-22 20:33:49 +0300

    Added image.lua

--- image.lua
+++ image.lua
@@ -1,21 +1,39 @@
---[[
-
-]]
-
 Image = {}
 Image.mt = {}
-Image.data = {}
 
 setmetatable(Image, {
-    __call = function(_, path, x, y, mode, pl)
-        if str then
-            return player(id, str)
-        elseif Player.data[id] then
-            return Player.data[id]
-        else
-            local p = setmetatable({id = id}, Player.mt)
-            Player.data[id] = p
-            return p
-        end
+    __call = function(_, ...)
+        return setmetatable({id = image(unpack(arg))}, Image.mt)
     end
 })
+
+local transform = {
+    free = 'freeimage',
+    alpha = 'imagealpha',
+    blend = 'imageblend',
+    color = 'imagecolor',
+    hitzone = 'imagehitzone',
+    pos = 'imagepos',
+    scale = 'imagescale',
+    t_alpha = 'tween_alpha',
+    t_color = 'tween_color',
+    t_move = 'tween_move',
+    t_rotate = 'tween_rotate',
+    t_rotateconstantly = 'tween_rotateconstantly',
+    t_scale = 'tween_scale'
+}
+
+for k,v in pairs(transform) do  -- generate methods from transform table
+    Image.mt[k] = function(self, ...)
+        _G[v](self.id, unpack(arg))
+    end
+end
+
+function Image.mt:__index(key)
+    local m = rawget(Image.mt, key)
+    if m then
+        return m
+    else
+        error("Unknown method " .. key)
+    end
+end
--- lapi.lua
+++ lapi.lua
@@ -1,3 +1,4 @@
 dofile("sys/lua/lapi/player.lua")
 dofile("sys/lua/lapi/object.lua")
+dofile("sys/lua/lapi/image.lua")
 dofile("sys/lua/lapi/hook.lua")
