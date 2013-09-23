787be72e7c765db6300f762a4061008045f61111
tarjoilija <admin@awly.org>
2013-09-23 12:21:37 +0300

    Added Game

---
+++ game.lua
@@ -0,0 +1,10 @@
+Game = {}
+
+setmetatable(Game, {
+    __index = function(_, key)
+        return game(key)
+    end,
+    __newindex = function(_, key, value)
+        parse(key .. " " .. value)
+    end
+})
--- lapi.lua
+++ lapi.lua
@@ -4,6 +4,7 @@ if not lapi then
     dofile("sys/lua/lapi/image.lua")
     dofile("sys/lua/lapi/hook.lua")
     dofile("sys/lua/lapi/map.lua")
+    dofile("sys/lua/lapi/game.lua")
     lapi = {
         version = "0.1"
     }
